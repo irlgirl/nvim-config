18f9ba8bf5f0c7bc969011faaf0df6e9978c10cc
Andrey Golovatskiy <a.golovatskiy@corp.vk.com>
2024-05-16 16:34:10 +0300

    fixup ca

--- init.lua
+++ init.lua
@@ -498,7 +498,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>D', builtin.lsp_type_definitions, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', function ()
+      if vim.bo.filetype == 'rust' then
+        vim.cmd.RustLsp('codeAction')
+      else
+        vim.lsp.buf.code_action()
+      end
+    end, opts)
+
     vim.keymap.set('v', 'ff', '<ESC><cmd> lua vim.lsp.buf.format()<CR>', opts)
     vim.keymap.set('n', 'ff', function()
       vim.lsp.buf.format { async = true }
