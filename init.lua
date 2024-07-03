e8dd480a45b967c7b3b7156a0393a015b777f03f
Andrei Golovatskii <golovatskii.ak@gmail.com>
2024-07-03 15:22:34 +0300

    fixup neotree

--- init.lua
+++ init.lua
@@ -281,12 +281,14 @@ require("neo-tree").setup({
 	  },
   },
   event_handlers = {
+    --[[
     {
       event = "file_opened",
       handler = function(file_path)
         require("neo-tree.command").execute({ action = "close" })
       end
     },
+    ]]--
   },
   default_component_configs = {
     file_size = {
